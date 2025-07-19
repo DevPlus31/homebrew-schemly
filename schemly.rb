9f91de656d03ef8eb6ee7bbe84f07d22e7804cd3
Mustapha Ben <devplus31@gmail.com>
2025-07-19 21:50:38 +0100

    update version

--- schemly.rb
+++ schemly.rb
@@ -3,7 +3,7 @@ class Schemly < Formula
   homepage "https://github.com/DevPlus31/schemly"
   url "https://github.com/DevPlus31/schemly/releases/download/v0.8.0/schemly.0.8.0-macos-tar.gz"
   sha256 "acd0e7d0545dce1973255038fbc0a9c0e8c0ea9d47ea132a210944a892cd5c19"
-  version "1.0.0"
+  version "0.8.0"
 
   def install
     bin.install "schemly"
